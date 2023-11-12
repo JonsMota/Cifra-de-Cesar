848e08f30e972d3048bb21ba7e5cd9f4830e117f
JonsMota <jonsmota@gmail.com>
2023-11-12 17:49:15 -0300

    feito

---
+++ cifra.py
@@ -0,0 +1,31 @@
+print("Digite sua mensagem:")
+
+def cifrar_caractere(caractere, seq, chave):
+    indice_atual = seq.index(caractere)
+    novo_indice = indice_atual + chave
+
+    while novo_indice >= len(seq):
+        novo_indice = novo_indice - len(seq)
+  
+    while novo_indice < 0:
+        novo_indice = novo_indice + len(seq)
+    return seq[novo_indice]
+
+texto = (input())
+chave = 3
+
+minusculas = 'abcdefghijklmnopqrstuvwxyz'
+maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+cifra = ''
+
+for caractere in texto:
+    if caractere in minusculas:
+        caractere_cifra = cifrar_caractere(caractere, minusculas, chave)
+    elif caractere in maiusculas:
+        caractere_cifra = cifrar_caractere(caractere, maiusculas, chave)
+    else:
+        caractere_cifra = caractere
+    cifra += caractere_cifra
+
+print(cifra)
